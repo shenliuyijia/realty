7c93365ffbf469bea64ec9fe372791fb0f127aca
shenchenchen_glb <shenchenchen_glb@kgc.com>
2020-11-25 19:21:19 +0800

    页面退出功能

--- src/main/java/com/kgc/git2018/controller/UsersController.java
+++ src/main/java/com/kgc/git2018/controller/UsersController.java
@@ -6,9 +6,15 @@ import com.kgc.git2018.pojo.RealExample;
 import com.kgc.git2018.service.RealService;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 /**
  * @author shkstart
@@ -19,9 +25,11 @@ public class UsersController {
     @Resource
     RealService realService;
 
-    @RequestMapping("show")
-    public PageInfo<Real> selectAll(RealExample realExample, Integer pageNum, Integer pageSize) {
-        return null;
+    //退出
+    @RequestMapping("logout.do")
+    public String logout(HttpSession session,Model model){
+        session.invalidate();
+        return "redirect:login.jsp";
     }
 
 }
